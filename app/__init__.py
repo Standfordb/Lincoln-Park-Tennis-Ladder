c0a015dbf5e25035fbafc4f8f6765dd13907f859
Benjamin Standford <BenjaminStandford@gmail.com>
2023-10-12 16:44:17 -0400

    Quick Fix

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,9 @@ from app.extensions import socketio, Migrate, SQLAlchemy, Flask
 app = Flask(__name__)
 app.secret_key = "Sb39MDCIyj1kWgEKVzpmkQ"
 # Production Server:
-#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Wj7gWQuu849VNDMYSY3j@containers-us-west-191.railway.app:7456/railway"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Wj7gWQuu849VNDMYSY3j@containers-us-west-191.railway.app:7456/railway"
 # Development Server:
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:h6CIkVq5BOGYvuf9bYWd@containers-us-west-76.railway.app:6936/railway"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:h6CIkVq5BOGYvuf9bYWd@containers-us-west-76.railway.app:6936/railway"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.app_context().push()
 db = SQLAlchemy(app)
